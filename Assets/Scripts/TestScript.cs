d0f769f2abb7b8d2c7f0232545f09a395078f5b9
KoshiUchida <it242047@trident.ac.jp>
2025-01-30 21:41:04 +0900

    カメラの操作機能を搭載

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -13,6 +13,37 @@ public class TestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.0f, 0.0f, 5.0f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, 0.0f, -5.0f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(5.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-5.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.Rotate(45.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.Rotate(-45.0f * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.Rotate(0.0f, 45.0f * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            transform.Rotate(0.0f, -45.0f * Time.deltaTime, 0.0f);
+        }
     }
 }
